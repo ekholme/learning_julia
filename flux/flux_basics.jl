363326718ca73f8720c063da470de3c83519817f
Eric Ekholm <eric.ekholm@gmail.com>
2022-08-31 06:54:12 -0400

    finish mle blog

--- flux/flux_basics.jl
+++ flux/flux_basics.jl
@@ -10,3 +10,6 @@ x_train, x_test = hcat(0:5...), hcat(6:10...)
 
 y_train, y_test = actual.(x_train), actual.(x_test)
 
+#set up basic model
+model = Dense(1 => 1)
+#2 params -- 1 weight and one bias
