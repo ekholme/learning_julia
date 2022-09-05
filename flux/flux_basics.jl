8cb124d6da554aa3c38898888159b9a7be2bb41d
Eric Ekholm <eric.ekholm@gmail.com>
2022-09-05 14:50:55 -0400

    basics walkthrough flux

--- flux/flux_basics.jl
+++ flux/flux_basics.jl
@@ -1,4 +1,5 @@
 using Flux
+using Flux: train!
 
 #see https://fluxml.ai/Flux.jl/stable/models/overview/ to start
 
@@ -13,3 +14,44 @@ y_train, y_test = actual.(x_train), actual.(x_test)
 #set up basic model
 model = Dense(1 => 1)
 #2 params -- 1 weight and one bias
+
+#explore default parameters
+model.weight
+model.bias
+
+#and since we have default weights and biases, we can run our data through this initial model
+predict = Dense(1 => 1)
+predict(x_train)
+#the above is the same as predict.weight * x_train .+ predict.bias
+
+#to train the function, we need to specify a loss function
+loss(x, y) = Flux.Losses.mse(predict(x), y)
+
+#initial loss
+loss(x_train, y_train)
+
+#we also need to provide an optimizer to train
+#in this case, regular gradient descent
+opt = Descent()
+
+#create a tuple with x and y
+data = [(x_train, y_train)]
+
+#create an object that holds all of our parameters
+parameters = Flux.params(predict)
+
+#and then we can run one training pass through all of this
+train!(loss, parameters, data, opt)
+
+#and we should be able to see that the loss went down and the parameters were updated
+loss(x_train, y_train)
+parameters
+
+#and if we want to run multiple passes
+for epoch in 1:200
+    train!(loss, parameters, data, opt)
+end
+
+#these are super close to what we specified
+loss(x_train, y_train)
+parameters
