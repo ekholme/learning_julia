7014d2bf2188f4f06b1c02f0af5b7f45ef73f213
Eric Ekholm <eric.ekholm@gmail.com>
2022-10-11 16:27:24 -0400

    more tt practice

--- tidytuesday/product_hunt.jl
+++ tidytuesday/product_hunt.jl
@@ -3,9 +3,10 @@ using DataFrames
 using Statistics
 using CairoMakie
 using Chain
+using Dates
 
 #read data in
-products = CSV.read(download("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2022/2022-10-04/product_hunt.csv"), DataFrame, missingstring = "NA")
+products = CSV.read(download("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2022/2022-10-04/product_hunt.csv"), DataFrame, missingstring = "NA", dateformat="")
 
 #check out names of dataframe
 show(names(products))
@@ -22,4 +23,38 @@ prods = collect(skipmissing(products.:product_ranking))
 hist(prods)
 #ok, so a pretty even distribution
 
-#RESUME HERE
+#parse year and month of release date
+
+#write a function to extract specific elements
+function extract_time(x, t)
+
+    @assert t in ["year", "month"]
+
+    seg = if t == "year"
+        1:4
+    else
+        6:7
+    end
+
+    ret = parse(Int64, x[seg])
+
+    return(ret)
+end
+
+#and then parse out release year and release month
+products.:release_year = extract_time.(products.:release_date, "year")
+products.:release_month = extract_time.(products.:release_date, "month")
+
+show(names(products))
+
+hist(products.:release_year)
+
+#next -- do a line plot -- will need to aggregate data first
+yr_count = @chain begin products
+    groupby(:release_year)
+    combine(nrow => :count)
+end
+
+yr_count
+
+lines(yr_count.:release_year, yr_count.:count)
